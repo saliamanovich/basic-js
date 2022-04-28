68196f789ba811949bc5f2552c06388201a5be10
saliamanovich <saliamanovicha@gmail.com>
2022-04-28 10:40:08 +0300

    add: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,8 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(matrix) {
+  let mas = [];
+  console.log(matrix.flat().filter(function(number){
+    return number==='^^';
+  }));
+  mas = matrix.flat().filter(function(number){
+    return number==='^^';
+  });
+  console.log(mas.length);
+  return mas.length;
   throw new NotImplementedError('Not implemented');
+
   // remove line with error and write your code here
 }
 
