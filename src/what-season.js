b77745425008dbfeaabb484912c4e6084e72ddf2
saliamanovich <saliamanovicha@gmail.com>
2022-05-01 10:41:36 +0300

    what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(date) {
+  /*console.log(date);
+  console.log(date.toString());
+  console.log(Date.parse(date));
+  /*console.log(date.toISOString());*/
+  /*console.log(date && date.getMonth && date.getMonth.call);
+  console.log(Object.prototype.toString.call(date)==='[object Date]');
+  console.log(typeof(date));
+  console.log(date instanceof Date);
+  console.log(date.getSeconds());*/
+  if (arguments.length===0){
+    console.log('null');
+    return 'Unable to determine the time of year!';
+  }
+  if (isNaN(Date.parse(date))){
+    console.log('invalid date!') 
+    return "Invalid date!";
+  }
+  
+  else if (Object.prototype.toString.call(date) !== '[object Date]'){
+    console.log(Object.prototype.toString.call(date) === '[object Date]');
+    return "Invalid date!";
+  }
+  else if (!date instanceof Date){
+    return "Invalid date!";
+  }
+
+  /*else if (!date.getSeconds()){
+    return 'Invalid date!';
+  }*/
+  /*else if (typeof(date)!==Date){
+    return 'Invalid date!';
+  }*/
+  else {
+  let month = date.getMonth();
+  switch (month){
+    case 0:
+    case 1:
+    case 11:
+      console.log('winter');
+      return 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      console.log('spring');
+      return 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      console.log('summer');
+      return 'summer';
+      break;
+    case 8:
+    case 9:
+    case 10:
+      console.log('autumn (fall)');
+      return 'autumn (fall)';
+      break;
+    default:
+      console.log('Unable to determine the time of year');
+      return "Invalid date!";
+  }
+  }
+ 
+  //return date;
   throw new NotImplementedError('Not implemented');
+  
   // remove line with error and write your code here
 }
+getSeason('foo'),
+getSeason({ John: 'Smith' }),
+getSeason(20192701),
+getSeason([2019, '27', 0 + '1']),
+getSeason(() => new Date())
 
 module.exports = {
   getSeason
